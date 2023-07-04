8e1d09041ebc5a8086f307ed48050d5af02ade54
knpravirawat111 <knpravirawat111@gmail.com>
2023-07-04 23:33:50 +0530

    5th Question upload

---
+++ Mocktest 2/Ques5.java
@@ -0,0 +1,17 @@
+public class Ques5{
+    public static void main(String[] args) {
+        // Integer 
+        int age = 25;
+
+        // Double 
+        double salary = 2500.50;
+
+        // String 
+        String name = "John Doe";
+
+        // Printing the values of the variables
+        System.out.println("Age is " + age);
+        System.out.println("Salary is " + salary);
+        System.out.println("Name is " + name);
+    }
+}
